1467a89e3d8a51451b3a93a7a0b27a2520be26c3
Yomex <vonscream@outlook.com>
2019-03-20 10:53:49 +0800

    Learn of Design Mode

--- src/Facade/Client.java
+++ src/Facade/Client.java
@@ -4,6 +4,7 @@ package Facade;
 public class Client {
     public static void main(String[] args){
         Facade facade = new Facade();
+
         facade.MethodA();
         facade.MethodB();
     }
